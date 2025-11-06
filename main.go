6afacbeb482ba1d35ca216232422140b512f0a9a
swag2044 <olzhassovetbekov130207@gmail.com>
2025-11-06 19:25:30 +0500

    ad

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import "fmt"
+
+func main() {
+	println("Task 1")
+
+	var a rune
+	for a = 48; a <= 57; a++ {
+		fmt.Printf("%c", a)
+	}
+
+	println("\nTask 2")
+
+	var b rune
+	for b = 97; b <= 122; b++ {
+		fmt.Printf("%c", b)
+	}
+
+	println("\nTask 3")
+
+	var c rune
+	for c = 57; c >= 48; c-- {
+		fmt.Printf("%c", c)
+	}
+
+	println("\nTask 4")
+
+	var d rune
+	for d = 122; d >= 97; d-- {
+		fmt.Printf("%c", d)
+	}
+}
